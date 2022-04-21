4e756d2771ba5de8b9f34f534a9673ae9cea109a
Manjunath D S <94745021+BigBro7@users.noreply.github.com>
2022-04-21 10:21:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ class HelloWorld
     public static void main(String args[])
     {
       System.out.println("welcome to Java 8 Stream");
-        System.out.println("hi I am fine");
+        System.out.println("This is Manjunath");
         int a,b,c,d;
         a=10;
         b=20;
